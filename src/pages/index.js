e38117dbf7a94bc6f17dcd7d618d3a90ae50a43f
Rakeeza <rakeeza.malik@and.digital>
2021-10-01 13:34:36 +0100

    cleaned up style sheets and installed MUI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import {Card} from 'antd';
-import 'antd/dist/antd.css';
+import Moonlight from '../images/moonlight.png'
 
 const IndexPage = () => {
   return (
@@ -8,7 +7,7 @@ const IndexPage = () => {
       <title>Keez To My Gallery</title>
       <h1>Keez To My Gallery</h1>
       <p>An exploration of my interests and hobbies</p>
-      <Card hoverable style={{ width: 240 }} title="Blog 1" cover={<img alt="moon through clouds" src="../images/moonlight.png" />}/>
+      <Card hoverable style={{ width: 240 }} title="Blog 1" cover={<img alt="moon through clouds" src={Moonlight} />}/>
     </main>
   )
 }
