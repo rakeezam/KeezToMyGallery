9cab3f249ad6b29078669b5906bf82d2ba211816
Rakeeza <rakeeza.malik@and.digital>
2021-10-04 13:33:47 +0100

    added an app bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,46 @@
 import * as React from 'react'
+import { AppBar, Card, CardMedia, CardContent, Typography, Toolbar, IconButton, Button } from '@material-ui/core';
 import Moonlight from '../images/moonlight.png'
+// import MenuIcon from '@material-ui/icons/Menu';
 
 const IndexPage = () => {
   return (
+    <>
     <main>
       <title>Keez To My Gallery</title>
       <h1>Keez To My Gallery</h1>
       <p>An exploration of my interests and hobbies</p>
-      <Card hoverable style={{ width: 240 }} title="Blog 1" cover={<img alt="moon through clouds" src={Moonlight} />}/>
+      <AppBar position="static"/> 
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6">
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      <AppBar /> 
+    
+  
+    {/* <Card sx={{ maxWidth: 345 }} >
+      <CardMedia
+        component="img"
+        height="140"
+        // image={Moonlight}
+        alt="green iguana"
+      />
+      <CardContent>
+       
+          <p> Lizard </p>
+     
+         <p> Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica </p>
+  
+      </CardContent>
+    </Card> */}
     </main>
+    </>
   )
 }
 
